e7bc5146f36e64d27585df51ac8393f8aa082c16
janelb <894353426@qq.com>
2018-07-09 15:39:16 +0800

    提交文件

---
+++ qq-app/src/main/java/com/libang/test/Test.java
@@ -0,0 +1,17 @@
+package com.libang.test;
+
+
+
+/**
+ * @author libang
+ * @date 2018/7/9 14:08
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+
+
+
+}
