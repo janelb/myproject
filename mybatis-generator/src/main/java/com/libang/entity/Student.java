15a0d0e06ae6359e1f2c678d0ee579be21ae61ec
janelb <894353426@qq.com>
2018-07-12 17:50:34 +0800

    mybatis逆向工程配置

---
+++ mybatis-generator/src/main/java/com/libang/entity/Student.java
@@ -0,0 +1,33 @@
+package com.libang.entity;
+
+public class Student {
+    private Integer id;
+
+    private String stuName;
+
+    private Integer schoolId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getStuName() {
+        return stuName;
+    }
+
+    public void setStuName(String stuName) {
+        this.stuName = stuName;
+    }
+
+    public Integer getSchoolId() {
+        return schoolId;
+    }
+
+    public void setSchoolId(Integer schoolId) {
+        this.schoolId = schoolId;
+    }
+}
---
+++ mybatis-generator/src/main/java/com/libang/util/MybatisUtils.java
@@ -0,0 +1,45 @@
+package com.libang.util;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+
+/**
+ * @author libang
+ * @date 2018/7/12 17:43
+ */
+public class MybatisUtils {
+    private static SqlSessionFactory sqlSessionFactory;
+    static {
+        Reader reader=null;
+        try {
+            reader  = Resources.getResourceAsReader("mybatis.xml");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+    }
+
+    public static SqlSessionFactory getSqlSessionFactory(){
+
+        return sqlSessionFactory;
+
+    }
+
+    public static SqlSession getSqlSession(boolean autoCommit){
+        return getSqlSessionFactory().openSession(autoCommit);
+
+    }
+    public  static SqlSession getSqlSession(){
+        return getSqlSessionFactory().openSession();
+
+    }
+
+
+
+}
