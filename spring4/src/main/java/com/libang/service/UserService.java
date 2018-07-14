d2e248313a65660ce09ec7bd5feacefee65b5340
janelb <894353426@qq.com>
2018-07-14 14:28:15 +0800

    自动注入

--- spring4/src/main/java/com/libang/service/UserService.java
+++ spring4/src/main/java/com/libang/service/UserService.java
@@ -13,16 +13,14 @@ public class UserService {
     public void save(){
         userDao.save();
     }
-    public UserService(UserDao userDao){
+/*    public UserService(UserDao userDao){
         this.userDao = userDao;
 
-
-
-    }
-
-/*    public void setUserDao(UserDao userDao) {
-        this.userDao = userDao;
     }*/
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
 
 
 }
