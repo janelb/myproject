c997af9d3c9e039f176ac5de5d642b0affe7fe0d
janelb <894353426@qq.com>
2018-07-14 14:21:29 +0800

    构造方法注入

--- spring4/src/main/java/com/libang/service/UserService.java
+++ spring4/src/main/java/com/libang/service/UserService.java
@@ -13,11 +13,16 @@ public class UserService {
     public void save(){
         userDao.save();
     }
-
-
-    public void setUserDao(UserDao userDao) {
+    public UserService(UserDao userDao){
         this.userDao = userDao;
+
+
+
     }
+
+/*    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }*/
 
 
 }
