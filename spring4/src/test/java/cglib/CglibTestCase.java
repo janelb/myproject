b14c218a47758b9069e2969a40d6878118305ec4
janelb <894353426@qq.com>
2018-07-16 11:26:10 +0800

    使用内部类的形式 建Cglib

--- spring4/src/test/java/cglib/CglibTestCase.java
+++ spring4/src/test/java/cglib/CglibTestCase.java
@@ -3,7 +3,11 @@ package cglib;
 import com.libang.cglib.Sale;
 import com.libang.cglib.SubjectMethodInterceptor;
 import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
 import org.junit.Test;
+
+import java.lang.reflect.Method;
 
 /**
  * @author libang
@@ -25,4 +29,41 @@ public class CglibTestCase {
         System.out.println(sale.getClass().getName());
         sale.salePc();
     }
+
+
+    //方法二：使用内部类的形式实现动态代理
+    @Test
+    public void testCglib2(){
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(Sale.class);
+        enhancer.setCallback(new MethodInterceptor() {
+            /**
+             * @param target 目标对象
+             * @param method
+             * @param args 参数列表
+             * @param methodProxy 产生父类方法的代理对象
+             * @return
+             * @throws Throwable
+             */
+            @Override
+            public Object intercept(Object target, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
+
+                System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+                //执行父类（目标对象） 的方法
+                Object result = methodProxy.invokeSuper(target,args);
+                System.out.println("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
+
+                return result;
+            }
+        });
+        //动态产生动态代理对象
+        Sale sale = (Sale) enhancer.create();
+        sale.salePc();
+
+
+
+    }
 }
+
+
+
