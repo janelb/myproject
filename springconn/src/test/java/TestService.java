c1591d9d6d31dccdb7956656b0690aff1a72323c
janelb <894353426@qq.com>
2018-07-17 10:41:44 +0800

    注解方法分开，在测试类中

--- springconn/src/test/java/TestService.java
+++ springconn/src/test/java/TestService.java
@@ -15,12 +15,12 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * @date 2018/7/16 17:54
  */
 
-//第一种：基于Di的Spring单元测试类进行注解方式来开启容器，加载bean
 /*@RunWith(SpringJUnit4ClassRunner.class) //在junit运行时加载SpringJunit模块
 *//*@ContextConfiguration(locations = "classpath:properties.xml")*//*//从classpath路径下读取spring配置文件，并创建spring容器
 
 //第二种方法：配置类替换配置文件，配置文件的路径在com.libang下，
-@ContextConfiguration(classes = Application.class)*///基于配置类的详情类来加载配置文件开启容器
+@ContextConfiguration(classes = Application.class)*///基于配置类的详情
+//第一种：基于Di的Spring单元测试类进行注解方式来开启容器，加载bean类来加载配置文件开启容器
 public class TestService extends BaseTestCase {
 
     //直接注入对应的Bean
@@ -38,11 +38,37 @@ public class TestService extends BaseTestCase {
       /*  ApplicationContext context = new ClassPathXmlApplicationContext("properties.xml");
         UserService userService = (UserService) context.getBean("userService");*/
 
-
         userService.save();
-
     }
 
 
 
 }
+
+/**
+ *
+ /*@RunWith(SpringJUnit4ClassRunner.class) //在junit运行时加载SpringJunit模块
+ @ContextConfiguration(classes = Application.class)*//*//基于配置类的详情
+ public class TestService extends BaseTestCase {
+
+    //直接注入对应的Bean
+    @Autowired
+    private UserService userService;
+
+    @Test*/
+//第二种方法：配置类替换配置文件，配置文件的路径在com.libang下，
+
+/*    public void testService(){
+
+        //基于配置类进行开启容器获取bean
+       *//* ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
+       UserService userService = (UserService)context.getBean("userService");*//*
+        userService.save();
+    }
+
+}*/
+
+
+
+
+
