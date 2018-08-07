4714e70b9aced26642f0e5fb8d5b6c8ae218f573
janelb <894353426@qq.com>
2018-08-07 15:11:12 +0800

    手动签收消息，设置持久模式，自定义重置参数

--- jms-test/src/test/java/JmsTestCsae.java
+++ jms-test/src/test/java/JmsTestCsae.java
@@ -1,4 +1,5 @@
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.RedeliveryPolicy;
 import org.junit.Test;
 
 import javax.jms.*;
@@ -17,39 +18,70 @@ public class JmsTestCsae {
      * @throws JMSException
      */
     @Test
-    public void messaageProducter() throws JMSException {
-        //创建连接工厂
-        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
-        //创建连接并开启
-        Connection connection = connectionFactory.createConnection();
-        connection.start();
-        //创建session  param1:是否开启手动事务提交，如果是false为自动提交， param2:接收者签收模式：AUTO_ACKNOWLEDGE(自动签收)，CLIENT_ACKNOWLEDGE(手动签收）
-        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-        //创建消息目的地
-        Destination destination = session.createQueue("helloWord-1");
-        //创建生产者
-        MessageProducer messageProducer = session.createProducer(destination);
-        //发送消息
-        TextMessage textMessage = session.createTextMessage("hello1");
-        messageProducer.send(textMessage);
+    public void messaageProducter() throws Exception {
+        Connection connection = null;
+        MessageProducer messageProducer = null;
+        Session session = null;
+        try {
+            //创建连接工厂
+            ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+            //创建连接并开启
+            connection = connectionFactory.createConnection();
+            connection.start();
+            //创建session  param1:是否开启手动事务提交，如果是false为自动提交， param2:接收者签收模式：AUTO_ACKNOWLEDGE(自动签收)，CLIENT_ACKNOWLEDGE(手动签收）
+            session = connection.createSession(true, Session.CLIENT_ACKNOWLEDGE);//手动进行签收
+            //创建消息目的地
+            Destination destination = session.createQueue("helloWord-1");
+            //创建生产者
+            messageProducer = session.createProducer(destination);
+            //设置持久模式（没有被消费的消息，即使系统重新启动也不会消失）
+            messageProducer.setDeliveryMode(DeliveryMode.PERSISTENT);
 
-        //关闭资源
-        messageProducer.close();
-        session.close();
-        connection.close();
+            //发送消息
+            TextMessage textMessage = session.createTextMessage("hello1");
+            messageProducer.send(textMessage);
+
+            //进行手动提交事务
+            session.commit();
+
+        } catch (JMSException e) {
+
+        } finally {
+            //关闭资源
+            messageProducer.close();
+            session.close();
+            connection.close();
+
+        }
 
     }
+
 
     @Test
     public void messaegCustomer() throws JMSException, IOException {
 
         //创建连接工厂
         ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(ActiveMQConnectionFactory.DEFAULT_BROKER_URL);
+
+/*
+        //自定义重置参数
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+
+        RedeliveryPolicy redeliveryPolicy = new RedeliveryPolicy();
+        //设置重置次数
+        redeliveryPolicy.setMaximumRedeliveries(3);
+        //设置初次重置延迟时间，单位毫秒
+        redeliveryPolicy.setInitialRedeliveryDelay(2000);
+        //设置每次重置延迟时间，单位毫秒
+        redeliveryPolicy.setRedeliveryDelay(2000);
+        connectionFactory.setRedeliveryPolicy(redeliveryPolicy);*/
+
+
         //创建并启动连接
         Connection connection = connectionFactory.createConnection();
         connection.start();
         //创建session
-        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        final Session session = connection.createSession(false, Session.CLIENT_ACKNOWLEDGE);//手动签收消息
         //创建目的地对象
         Destination destination = session.createQueue("helloWord-1");
         //创建消费者
@@ -58,12 +90,28 @@ public class JmsTestCsae {
         //获取消息
         messageConsumer.setMessageListener(new MessageListener() {
             public void onMessage(Message message) {
-                TextMessage textMessage = (TextMessage) message;
 
                 try {
+                    TextMessage textMessage = (TextMessage) message;
                     System.out.println(textMessage.getText());
+
+                    //为使报错添加条件
+                    if("hello1".equals(textMessage.getText())){
+                        throw new JMSException("error");
+                    }
+
+                    //手动签收消息
+                    textMessage.acknowledge();
+
                 } catch (JMSException e) {
+
                     e.printStackTrace();
+                    //重新接受消息
+                    try {
+                        session.recover();
+                    } catch (JMSException e1) {
+                        e1.printStackTrace();
+                    }
                 }
             }
         });
