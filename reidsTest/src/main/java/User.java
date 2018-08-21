bba57047bcad761b0779513ce82bd8edc76ab743
janelb <894353426@qq.com>
2018-08-21 14:14:23 +0800

    java连接redis，json 连接池  数组封装

---
+++ reidsTest/src/main/java/User.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+/**
+ * @author libang
+ * @date 2018/8/21 13:56
+ */
+public class User {
+}
---
+++ reidsTest/src/test/java/RedisTest.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+/**
+ * @author libang
+ * @date 2018/8/21 12:20
+ */
+public class RedisTest {
+}
