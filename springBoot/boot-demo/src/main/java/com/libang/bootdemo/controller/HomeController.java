6e72fdbe5d14c467c25fbc4e27a3e1282c83ca2c
janelb <894353426@qq.com>
2018-08-22 20:55:03 +0800

    springBoot 简单操作

---
+++ springBoot/boot-demo/src/main/java/com/libang/bootdemo/controller/HomeController.java
@@ -0,0 +1,8 @@
+package com.libang.bootdemo.controller;
+
+/**
+ * @author libang
+ * @date 2018/8/22 17:13
+ */
+public class HomeController {
+}
---
+++ springBoot/springBoot-demo/src/main/java/com/libang/springBootdemo/controller/HomeController.java
@@ -0,0 +1,8 @@
+package com.libang.springBootdemo.controller;
+
+/**
+ * @author libang
+ * @date 2018/8/22 16:59
+ */
+public class HomeController {
+}
